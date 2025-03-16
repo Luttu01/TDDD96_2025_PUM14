46d89233315998e6402b7aefdeaef333c3f8d119
marhe598 <marhe598@student.liu.se>
2025-03-16 20:30:50 +0100

    Lade till datum och fixade till overflow och textstorlek. Fixade också sådant att menyer ordentligt färgas

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -51,51 +51,63 @@
         journalmall = "Journalmall";
         vardenhet = "Vårdenhet";
         yrkesroll = "Yrkesroll";
+        const date1 = document.getElementById("DatumÄldst") as HTMLFormElement;
+        const date2 = document.getElementById("DatumSenast") as HTMLFormElement;
+        date1.value = "";
+        date2.value = "";
         update();
     }
 </script>
 
 <div class="flex display-center h-[3em] outline-solid outline-gray-300 bg-gray-100">
     <h1 id="ProjektTitel" class="pl-[5vw] pr-[5vw] text-4xl"><a href="/" on:click={reset}>Better<span class="text-purple-700">Care</span></a></h1>
-    <div id="Filtermenu" class="flex flex-row gap-[1vw] items-center text-2xl">
-
-
-        <div id="Sök" class="w-[10vw] overflow-hidden outline-3 outline-gray-300 rounded-xl bg-white">
+    <div id="Filtermenu" class="flex flex-row gap-[1vw] items-center text-[1.2em]">
+        <div id="Sök" class="w-[15vw] overflow-hidden outline-3 outline-gray-300 rounded-xl bg-white">
             <input class="pl-[5%] w-[100%] bg-white outline-3 outline-gray-300 rounded-xl" type="text" placeholder="Sök:">
         </div>
 
         <div id="Journalmall" class="w-[10vw] outline-3 outline-gray-300 rounded-xl bg-white">
-            <button id="dropdown_button" class="pl-[5%]">
-                {journalmall} <i class="fa fa-caret-down"></i>
-            </button>
+            <div id="dropdown_button">
+                <button class="pl-[5%] w-[80%] overflow-hidden">
+                    {journalmall}
+                </button>
+                <i class="fa fa-caret-down"></i>
+            </div>
+        
             <ul id="dropdown_1">
                 {#each journalmallar as journal}
                     <li>
-                        <button name={journal} on:click={handleButtonClick1}>{journal}</button>
+                        <button class="w-[100%] text-ellipsis" name={journal} on:click={handleButtonClick1}>{journal}</button>
                     </li>
                 {/each}
             </ul>
         </div>
         <div id="Vårdenhet" class="w-[10vw] outline-3 outline-gray-300 rounded-xl bg-white">
-            <button id="dropdown_button" class="pl-[5%]">
-                {vardenhet} <i class="fa fa-caret-down"></i>
-            </button>
+            <div id="dropdown_button">
+                <button class="pl-[5%] w-[80%] overflow-hidden">
+                    {vardenhet}
+                </button>
+                <i class="fa fa-caret-down"></i>
+            </div>
             <ul id="dropdown_2">
                 {#each vardenheter as enhet}
                     <li>
-                        <button name={enhet} on:click={handleButtonClick2}>{enhet}</button>
+                        <button class="w-[100%] text-ellipsis" name={enhet} on:click={handleButtonClick2}>{enhet}</button>
                     </li>
                 {/each}
             </ul>
         </div>
         <div id="Yrkesroll" class="w-[10vw] outline-3 outline-gray-300 rounded-xl bg-white">
-            <button id="dropdown_button" class="pl-[5%]">
-                {yrkesroll} <i class="fa fa-caret-down"></i>
-            </button>
+            <div id="dropdown_button">
+                <button class="pl-[5%] w-[80%] overflow-hidden">
+                    {yrkesroll}
+                </button>
+                <i class="fa fa-caret-down"></i>
+            </div>
             <ul id="dropdown_3">
                 {#each yrkesroller as yrke}
-                    <li>
-                        <button name={yrke} on:click={handleButtonClick3}>{yrke}</button>
+                    <li class="display: inline">
+                        <button class="w-[100%]" name={yrke} on:click={handleButtonClick3}>{yrke}</button>
                     </li>
                 {/each}
             </ul>
@@ -111,6 +123,8 @@
             #dropdown_button {
                 color: #000;
                 text-decoration: none;
+                white-space: nowrap;
+                overflow: hidden;
             }
             #dropdown_1, #dropdown_2, #dropdown_3 {
                 display: none;
@@ -121,12 +135,13 @@
                 text-decoration: none;
                 padding: 5px;
             }
-            #dropdown_1 button:hover, #dropdown_2 button:hover, #dropdown_3 button:hover {background-color: #ddd;}
+            #dropdown_1 button:hover, #dropdown_2 button:hover, #dropdown_3 button:hover {background-color: #9E7BB3;}
 
             #Journalmall:hover ul, #Yrkesroll:hover ul, #Vårdenhet:hover ul {
                 display: flex;
                 position: absolute;
                 flex-direction: column;
+                font-size: 15px;
                 background: white;
                 width: 90%;
                 margin-left: 5%;
@@ -134,12 +149,20 @@
                 box-shadow: 0px 20px 100px 0px rgba(0, 0, 0, 0.5);
             }
             #Journalmall:hover, #Yrkesroll:hover, #Vårdenhet:hover {
-                size-adjust: fit-content;
-                background-color: lightgray;
+                background-color: #9470B0;
             }
         </style>
-
+        <div id="DatumDiv" class="w-[21vw] h-[60%] outline-3 outline-gray-300 rounded-xl bg-white text-[80%] flex items-center">
+            <div id="Datum1" class="flex flex-row gap-[1vw] items-center">
+                <label for="DatumÄldst"></label>
+                <input type="date" name="DatumÄldst" id="DatumÄldst">
+            </div>
+            <p class="pl-[1vw]">-</p>
+            <div id="Datum2" class="flex flex-row gap-[1vw] items-center">
+                <label for="DatumSenast"></label>
+                <input type="date" name="DatumSenast" id="DatumSenast">
+            </div>
+        </div>
         <button id="Reset" class="hover:text-purple-500" on:click={reset}>Återställ</button>
-        <div>Matchande Dokument: {matchingDocs}</div>
     </div>
 </div>
