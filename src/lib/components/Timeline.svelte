3814c1b1ca94b070c8e9e97b0488ae3cfb871e60
GranneJanne <Wilja141@student.liu.se>
2025-03-31 11:58:43 +0200

    further code cleanup

--- src/lib/components/Timeline.svelte
+++ src/lib/components/Timeline.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { derived, writable } from "svelte/store";
-  import { flip } from 'svelte/animate';
-  import { fade } from 'svelte/transition';
+  import { flip } from "svelte/animate";
+  import { fade } from "svelte/transition";
 
   type Note = {
     date: Date;
@@ -27,28 +27,37 @@
     isCollapsed: boolean;
   };
 
-  let notes = writable<Note[]>([
+  let notes = writable([
     { date: new Date("2021-01-01"), content: "Anteckning 1" },
-    { date: new Date("2021-01-01"), content: "Anteckning 1" },
-    { date: new Date("2021-01-01"), content: "Anteckning 1" },
-    { date: new Date("2021-01-02"), content: "Anteckning 2" },
-    { date: new Date("2021-02-03"), content: "Anteckning 3" },
-    { date: new Date("2021-02-04"), content: "Anteckning 4" },
-    { date: new Date("2021-03-05"), content: "Anteckning 5" },
-    { date: new Date("2021-04-06"), content: "Anteckning 6" },
-    { date: new Date("2021-05-07"), content: "Anteckning 7" },
-    { date: new Date("2021-05-08"), content: "Anteckning 8" },
-    { date: new Date("2022-01-09"), content: "Anteckning 9" },
-    { date: new Date("2022-04-10"), content: "Anteckning 10" },
-    { date: new Date("2023-05-11"), content: "Anteckning 11" },
-    { date: new Date("2023-05-12"), content: "Anteckning 12" },
-    { date: new Date("2023-06-13"), content: "Anteckning 13" },
-    { date: new Date("2023-06-14"), content: "Anteckning 14" },
-    { date: new Date("2024-01-15"), content: "Anteckning 15" },
-    { date: new Date("2024-01-16"), content: "Anteckning 16" },
-    { date: new Date("2024-02-17"), content: "Anteckning 17" },
-    { date: new Date("2024-03-18"), content: "Anteckning 18" },
-    { date: new Date("2024-04-19"), content: "Anteckning 19" },
+    { date: new Date("2021-01-01"), content: "Anteckning 2" },
+    { date: new Date("2021-01-03"), content: "Anteckning 3" },
+    { date: new Date("2021-02-01"), content: "Anteckning 4" },
+    { date: new Date("2021-02-01"), content: "Anteckning 5" },
+    { date: new Date("2021-03-10"), content: "Anteckning 6" },
+    { date: new Date("2021-03-10"), content: "Anteckning 7" },
+    { date: new Date("2021-03-10"), content: "Anteckning 8" },
+    { date: new Date("2021-04-01"), content: "Anteckning 9" },
+    { date: new Date("2021-05-05"), content: "Anteckning 10" },
+    { date: new Date("2021-05-05"), content: "Anteckning 11" },
+    { date: new Date("2021-08-23"), content: "Anteckning 12" },
+    { date: new Date("2021-08-23"), content: "Anteckning 13" },
+    { date: new Date("2021-10-11"), content: "Anteckning 14" },
+    { date: new Date("2021-12-24"), content: "Anteckning 15" },
+    { date: new Date("2022-01-05"), content: "Anteckning 16" },
+    { date: new Date("2022-01-05"), content: "Anteckning 17" },
+    { date: new Date("2022-03-17"), content: "Anteckning 18" },
+    { date: new Date("2022-04-25"), content: "Anteckning 19" },
+    { date: new Date("2022-04-25"), content: "Anteckning 20" },
+    { date: new Date("2022-04-25"), content: "Anteckning 21" },
+    { date: new Date("2022-07-20"), content: "Anteckning 22" },
+    { date: new Date("2022-08-30"), content: "Anteckning 23" },
+    { date: new Date("2022-09-15"), content: "Anteckning 24" },
+    { date: new Date("2022-12-31"), content: "Anteckning 25" },
+    { date: new Date("2023-01-01"), content: "Anteckning 26" },
+    { date: new Date("2023-01-01"), content: "Anteckning 27" },
+    { date: new Date("2023-02-14"), content: "Anteckning 28" },
+    { date: new Date("2023-02-14"), content: "Anteckning 29" },
+    { date: new Date("2023-02-14"), content: "Anteckning 30" },
   ]);
 
   let scale: number = 1;
@@ -56,6 +65,7 @@
   const minScale: number = 0.5;
   const maxScale: number = 1.5;
   const baseWidth: number = 320;
+  const buttonPaddingX: number = 20;
 
   let enableTransition = writable(false);
   $: scale, enableTransition.set(false);
@@ -89,10 +99,10 @@
       dayGroup.notes.push(note);
     });
 
-    hierarchy.sort((a, b) => a.year - b.year);
-    hierarchy.forEach((y) => y.months.sort((a, b) => a.month - b.month));
+    hierarchy.sort((a, b) => b.year - a.year);
+    hierarchy.forEach((y) => y.months.sort((a, b) => b.month - a.month));
     hierarchy.forEach((y) =>
-      y.months.forEach((m) => m.days.sort((a, b) => a.day - b.day))
+      y.months.forEach((m) => m.days.sort((a, b) => b.day - a.day))
     );
 
     return hierarchy;
@@ -108,7 +118,6 @@
     setTimeout(() => enableTransition.set(true), 300);
     group.isCollapsed = !group.isCollapsed;
     noteHierarchy.update((n) => [...n]);
-    
   }
 
   function countVisibleNotes(groups: (Year | Month | Day)[]): number {
@@ -123,6 +132,14 @@
       }
     }
     return count;
+  }
+
+  function calculateWidth(group: Year | Month | Day) {
+    return (
+      baseWidth * countVisibleNotes([group]) * scale +
+      4 * countVisibleNotes([group]) -
+      buttonPaddingX * 2 * ("month" in group ? 2 : "day" in group ? 3 : 1)
+    );
   }
 
   const visibleNotes = derived(noteHierarchy, ($noteHierarchy) => {
@@ -200,44 +217,43 @@
 
 <div class="h-full bg-gray-100 flex overflow-x-auto">
   <div class="w-max flex-col flex p-2 space-y-2">
-    <div class="flex relative min-w-max px-[10px] justify-between" transition:fade={{ duration: 150 }}>
+    <div
+      class="flex relative min-w-max px-[{buttonPaddingX}px] justify-between"
+      transition:fade={{ duration: 150 }}
+    >
       {#each $noteHierarchy as yearGroup}
         <button
           on:click={(event) => toggleCollapse(yearGroup, event)}
           class="h-2 bg-purple-300 rounded-full"
           class:transition-width={$enableTransition}
-          style="width: {baseWidth * countVisibleNotes([yearGroup]) * scale +
-            4 * countVisibleNotes([yearGroup]) -
-            25}px;"
+          style="width: {calculateWidth(yearGroup)}px;"
           aria-label="Toggle year group"
         >
           {#if !yearGroup.isCollapsed}
-            <div class="flex justify-between px-[30px]" transition:fade={{ duration: 150 }}>
+            <div
+              class="flex justify-between px-[{buttonPaddingX}px]"
+              transition:fade={{ duration: 150 }}
+            >
               {#each yearGroup.months as monthGroup}
                 <!-- svelte-ignore node_invalid_placement_ssr -->
                 <button
                   on:click={(event) => toggleCollapse(monthGroup, event)}
                   class="h-2 bg-purple-500 rounded-full"
                   class:transition-width={$enableTransition}
-                  style="width: {baseWidth *
-                    countVisibleNotes([monthGroup]) *
-                    scale +
-                    4 * countVisibleNotes([monthGroup]) -
-                    85}px;"
+                  style="width: {calculateWidth(monthGroup)}px;"
                   aria-label="Toggle month group"
                 >
                   {#if !monthGroup.isCollapsed}
-                    <div class="flex justify-between px-[30px]" transition:fade={{ duration: 150 }}>
+                    <div
+                      class="flex justify-between px-[{buttonPaddingX}px]"
+                      transition:fade={{ duration: 150 }}
+                    >
                       {#each monthGroup.days as dayGroup}
                         <button
                           on:click={(event) => toggleCollapse(dayGroup, event)}
                           class="h-2 bg-purple-700 rounded-full"
                           class:transition-width={$enableTransition}
-                          style="width: {baseWidth *
-                            countVisibleNotes([dayGroup]) *
-                            scale +
-                            4 * countVisibleNotes([dayGroup]) -
-                            145}px;"
+                          style="width: {calculateWidth(dayGroup)}px;"
                           aria-label="Toggle day group"
                         >
                         </button>
@@ -253,33 +269,33 @@
     </div>
     <div class="flex flex-grow space-x-1">
       {#each $visibleNotes as item (item.type === "note" ? item.note?.date : item.text)}
-      <div class="flex flex-grow" animate:flip={{ duration: 250 }}>
-        {#if item.type === "summary"}
-          <button
-            transition:fade={{ duration: 150 }}
-            class="bg-gray-200 flex-none p-1 rounded-md shadow-sm text-gray-700"
-            style="width: {baseWidth * scale}px;"
-            on:click={() => {
-              if (item.year) toggleCollapse(item.year);
-              else if (item.month) toggleCollapse(item.month);
-              else if (item.day) toggleCollapse(item.day);
-            }}
-          >
-            {item.text}
-          </button>
-        {:else}
-          <div
-          transition:fade={{ duration: 150 }}
-            class="bg-white flex-none p-1 rounded-md shadow-sm"
-            style="width: {baseWidth * scale}px;"
-          >
-            <div class="text-left text-sm text-gray-500">
-              {item.note?.date.toLocaleDateString("sv-SE")}
+        <div class="flex flex-grow" animate:flip={{ duration: 250 }}>
+          {#if item.type === "summary"}
+            <button
+              transition:fade={{ duration: 150 }}
+              class="bg-gray-200 flex-none p-1 rounded-md shadow-sm text-gray-700"
+              style="width: {baseWidth * scale}px;"
+              on:click={() => {
+                if (item.year) toggleCollapse(item.year);
+                else if (item.month) toggleCollapse(item.month);
+                else if (item.day) toggleCollapse(item.day);
+              }}
+            >
+              {item.text}
+            </button>
+          {:else}
+            <div
+              transition:fade={{ duration: 150 }}
+              class="bg-white flex-none p-1 rounded-md shadow-sm"
+              style="width: {baseWidth * scale}px;"
+            >
+              <div class="text-left text-sm text-gray-500">
+                {item.note?.date.toLocaleDateString("sv-SE")}
+              </div>
+              {item.note?.content}
             </div>
-            {item.note?.content}
-          </div>
-        {/if}
-      </div>
+          {/if}
+        </div>
       {/each}
     </div>
   </div>
