cf14ac948ef999e054a58c7f9c94af7bf6b5f5e6
GranneJanne <Wilja141@student.liu.se>
2025-04-11 12:06:11 +0200

    Alternative timeline

--- src/lib/components/Timeline.svelte
+++ src/lib/components/Timeline.svelte
@@ -39,9 +39,11 @@
   }
 
   function calculateWidth(group: Year | Month | Day) {
-    const groupsNotesWidth = countVisibleNotesWithinGroup([group]) * (BASEWIDTH * scale + NOTESPACING);
+    const groupsNotesWidth =
+      countVisibleNotesWithinGroup([group]) * (BASEWIDTH * scale + NOTESPACING);
     // If the group is a month or day, we need to reduce width including it's parents padding
-    const groupsWidthReduction = DATEGROUPPADDING * ("month" in group ? 2 : "day" in group ? 3 : 1)
+    const groupsWidthReduction =
+      DATEGROUPPADDING * ("month" in group ? 2 : "day" in group ? 3 : 1);
 
     return groupsNotesWidth - groupsWidthReduction;
   }
@@ -55,12 +57,11 @@
   }
 
   function calculateNoteXPosition(item: { note?: Note; type: string }) {
-    const index = $visibleNotes.findIndex(n => n === item);
+    const index = $visibleNotes.findIndex((n) => n === item);
     const halfNoteWidth = (BASEWIDTH * scale) / 2;
-    const notePosition = index * ((BASEWIDTH * scale)+NOTESPACING);
+    const notePosition = index * (BASEWIDTH * scale + NOTESPACING);
     return notePosition + halfNoteWidth;
   }
-
 
   onMount(() => {
     window.addEventListener("wheel", handleZoom, { passive: false });
@@ -70,142 +71,65 @@
   });
 
   function handleNoteClick(noteData: Note) {
-    $selectedNotes = $selectedNotes || []; 
+    $selectedNotes = $selectedNotes || [];
 
-    const foundIndex = $selectedNotes.findIndex(n => n.CaseData === noteData.CaseData);
-
+    const foundIndex = $selectedNotes.findIndex(
+      (n) => n.CaseData === noteData.CaseData
+    );
 
     let newSelectedNotes = [...$selectedNotes];
 
     if (foundIndex >= 0) {
-    
       newSelectedNotes.splice(foundIndex, 1);
     } else {
-     
       newSelectedNotes.push(noteData);
     }
 
-   
     selectedNotes.set(newSelectedNotes);
   }
+
 </script>
 
-<div class="h-full bg-gray-100 flex overflow-x-auto no-scrollbar relative">
 
-  <!-- Timeline -->
-
-  <div class="absolute top-0 left-0 pointer-events-none z-10">
-    {#each $visibleNotes as item}
-    {#if item.type === "note"}
-        <div
-          class="absolute w-3 h-3 bg-black rounded-full outline-2 outline-gray-100 pointer-events-none"
-          style="
-            left: {calculateNoteXPosition(item)}px;
-            top: 0.5rem;
-          "
-        ></div>
-      {/if}
+<div class="relative h-full bg-gray-100 overflow-x-auto no-scrollbar p-4 pb-0">
+  <!-- Hierarchical Date Structure -->
+  <div class="relative flex space-x-4 h-full">
+    {#each $noteHierarchy as yearGroup}
+      <div class="relative bg-purple-200 p-2 pb-0 space-y-2 rounded-t-md flex flex-col flex-grow">
+        <div class="text-lg sticky left-0 w-20 font-bold text-gray-900">{yearGroup.year}</div>
+        {#if !yearGroup.isCollapsed}
+          <div class="flex space-x-4 flex-grow">
+            {#each yearGroup.months as monthGroup}
+              <div class="bg-purple-300 p-2 pb-0 space-y-2 rounded-t-md flex flex-col flex-grow">
+                <div class="text-md sticky left-0 w-20 font-medium text-gray-800">
+                  {new Date(0, monthGroup.month).toLocaleString("default", {month: "long"})}
+                </div>
+                {#if !monthGroup.isCollapsed}
+                  <div class="flex space-x-4 flex-grow">
+                    {#each monthGroup.days as dayGroup}
+                      <div class="bg-purple-400 p-2 pb-0 space-y-2 rounded-t-md flex flex-col">
+                        <div class="text-sm sticky left-0 w-4 text-gray-800">
+                          {dayGroup.day + 1}
+                        </div>
+                        <div class="flex space-x-2 pb-2 flex-grow">
+                        {#each dayGroup.notes as note}
+                          <div class="bg-white p-2 rounded-md w-80">
+                            <h3>{note.Dokumentnamn}</h3>
+                            <div class="text-xs text-gray-500">
+                              {note.Dokument_skapad_av_yrkestitel_Namn}
+                            </div>
+                          </div>
+                        {/each}
+                      </div>
+                      </div>
+                    {/each}
+                  </div>
+                {/if}
+              </div>
+            {/each}
+          </div>
+        {/if}
+      </div>
     {/each}
   </div>
-  <div class="w-max flex-col flex p-2 space-y-2">
-    <div
-      class="flex relative min-w-max px-[20px] justify-between"
-      transition:fade={{ duration: 150 }}
-    >
-      {#each $noteHierarchy as yearGroup}
-        <button
-          on:click={(event) => toggleCollapse(yearGroup, event)}
-          class="h-3 bg-purple-400 rounded-full outline-2 outline-gray-100"
-          class:transition-width={$enableTransition}
-          style="width: {calculateWidth(yearGroup)}px;"
-          data-date={yearGroup.year}
-          aria-label="Toggle year group"
-        >
-          {#if !yearGroup.isCollapsed}
-            <div
-              class="flex justify-between px-[20px]"
-              transition:fade={{ duration: 150 }}
-            >
-              {#each yearGroup.months as monthGroup}
-                <!-- svelte-ignore node_invalid_placement_ssr -->
-                <button
-                  on:click={(event) => toggleCollapse(monthGroup, event)}
-                  class="h-3 bg-blue-400 rounded-full outline-2 outline-gray-100"
-                  class:transition-width={$enableTransition}
-                  style="width: {calculateWidth(monthGroup)}px; opacity: {monthGroup.days.length > 1 ? 1 : 0}; pointer-events: {monthGroup.days.length > 1 ? 'auto' : 'none'}"
-                  data-date={monthGroup.month}
-                  aria-label="Toggle month group"
-                >
-                  {#if !monthGroup.isCollapsed}
-                    <div
-                      class="flex justify-between px-[20px]"
-                      transition:fade={{ duration: 150 }}
-                    >
-                      {#each monthGroup.days as dayGroup}
-                        <button
-                          on:click={(event) => toggleCollapse(dayGroup, event)}
-                          class="h-3 bg-green-400 rounded-full outline-2 outline-gray-100"
-                          class:transition-width={$enableTransition}
-                          style="width: {calculateWidth(dayGroup)}px; opacity: {dayGroup.notes.length > 1 ? 1 : 0}; pointer-events: {dayGroup.notes.length > 1 ? 'auto' : 'none'}"
-                          data-date={dayGroup.notes[0].DateTime}
-                          aria-label="Toggle day group"
-                        >
-                      </button>
-                      {/each}
-                    </div>
-                  {/if}
-                </button>
-              {/each}
-            </div>
-          {/if}
-        </button>
-      {/each}
-    </div>
-
-    <!-- Notes -->
-    <div class="flex flex-grow space-x-1 overflow-hidden">
-      {#each $visibleNotes as item (item.id)}
-        <div class="flex flex-grow overflow-hidden" animate:flip={{ duration: 250 }}>
-          {#if item.type === "summary"}
-            <button
-              transition:fade={{ duration: 150 }}
-              class="bg-gray-200 flex-none p-1 rounded-md shadow-sm text-gray-700 overflow-y-auto"
-              style="width: {BASEWIDTH * scale}px;"
-              on:click={() => {
-                if (item.context === "year") toggleCollapse(item.context);
-                else if (item.context === "month") toggleCollapse(item.context);
-                else if (item.context === "day") toggleCollapse(item.context);
-              }}
-            >
-              {item.text}
-            </button>
-          {:else}
-            <div
-              transition:fade={{ duration: 150 }}
-                class="flex-none p-4 rounded-md shadow-sm overflow-x-hidden { $selectedNotes?.find(n => n.CaseData === item.context.CaseData) ? 'bg-purple-100' : 'bg-white' }"
-              style="width: {BASEWIDTH * scale}px;"
-            >
-              <div class="text-left text-sm text-gray-500 flex justify-between">
-                {item.context.DateTime}
-                <button class="h-6 w-6 rounded-md fa text-white { $selectedNotes?.find(n => n.CaseData === item.context.CaseData) ? 'bg-red-500 fa-caret-down' : 'bg-green-500 fa-caret-up' }"
-                on:click={() => item.context.CaseData && handleNoteClick(item.context)} 
-              class:selected={$selectedNotes?.find(n => n.CaseData === item.context.CaseData)}
-              aria-label="select note"
-                ></button>
-              </div>
-              <div class="h-full overflow-y-auto">
-                {@html item.context.CaseData}
-                </div>
-            </div>
-          {/if}
-        </div>
-      {/each}
-    </div>
-  </div>
 </div>
-
-<style>
-  .transition-width {
-    transition: width 250ms ease-in-out;
-  }
-</style>
