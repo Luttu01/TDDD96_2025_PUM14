6e85355915bd72f37a6c01bc106b35fbbb8df851
GranneJanne <Wilja141@student.liu.se>
2025-04-11 13:52:30 +0200

    in progress (måste logga timmar)

--- src/lib/components/Timeline.svelte
+++ src/lib/components/Timeline.svelte
@@ -15,7 +15,7 @@
   let scale: number = 1;
   const ZOOMSPEED: number = 0.01;
   const MINSCALE: number = 0.5;
-  const MAXSCALE: number = 1.5;
+  const MAXSCALE: number = 3;
   const BASEWIDTH: number = 320;
   const DATEGROUPPADDING: number = 40;
   const NOTESPACING: number = 4;
@@ -87,49 +87,68 @@
 
     selectedNotes.set(newSelectedNotes);
   }
-
 </script>
 
-
-<div class="relative h-full bg-gray-100 overflow-x-auto no-scrollbar p-4 pb-0">
-  <!-- Hierarchical Date Structure -->
-  <div class="relative flex space-x-4 h-full">
-    {#each $noteHierarchy as yearGroup}
-      <div class="relative bg-purple-200 p-2 pb-0 space-y-2 rounded-t-md flex flex-col flex-grow">
-        <div class="text-lg sticky left-0 w-20 font-bold text-gray-900">{yearGroup.year}</div>
-        {#if !yearGroup.isCollapsed}
-          <div class="flex space-x-4 flex-grow">
-            {#each yearGroup.months as monthGroup}
-              <div class="bg-purple-300 p-2 pb-0 space-y-2 rounded-t-md flex flex-col flex-grow">
-                <div class="text-md sticky left-0 w-20 font-medium text-gray-800">
-                  {new Date(0, monthGroup.month).toLocaleString("default", {month: "long"})}
-                </div>
-                {#if !monthGroup.isCollapsed}
-                  <div class="flex space-x-4 flex-grow">
-                    {#each monthGroup.days as dayGroup}
-                      <div class="bg-purple-400 p-2 pb-0 space-y-2 rounded-t-md flex flex-col">
-                        <div class="text-sm sticky left-0 w-4 text-gray-800">
-                          {dayGroup.day + 1}
-                        </div>
-                        <div class="flex space-x-2 pb-2 flex-grow">
-                        {#each dayGroup.notes as note}
-                          <div class="bg-white p-2 rounded-md w-80">
-                            <h3>{note.Dokumentnamn}</h3>
-                            <div class="text-xs text-gray-500">
-                              {note.Dokument_skapad_av_yrkestitel_Namn}
+<div class="relative h-full bg-gray-100 overflow-x-auto no-scrollbar">
+  <div
+    class="relative flex space-x-4 h-full px-2 border-2 border-red-500"
+    style="width: calc(600vw * {scale});"
+  >
+    <!-- Hierarchical Date Structure -->
+    <div class="relative flex flex-grow space-x-4 h-full px-2">
+      {#each $noteHierarchy as yearGroup}
+        <div class="relative flex flex-col flex-grow">
+          <div class="flex bg-purple-200 py-1 px-6 rounded-b-full">
+            <div class="text-lg sticky left-0 w-20 font-bold text-gray-900">
+              {yearGroup.year}
+            </div>
+          </div>
+          {#if !yearGroup.isCollapsed}
+            <div class="flex space-x-4 flex-grow px-8">
+              {#each yearGroup.months as monthGroup}
+                <div class="flex flex-col flex-grow">
+                  <div class="flex bg-purple-300 py-1 px-6 rounded-b-full">
+                    <div
+                      class="text-md sticky left-0 w-20 font-medium text-gray-800"
+                    >
+                      {new Date(0, monthGroup.month).toLocaleString("default", {
+                        month: "long",
+                      })}
+                    </div>
+                  </div>
+                  {#if !monthGroup.isCollapsed}
+                    <div class="flex space-x-4 flex-grow px-8">
+                      {#each monthGroup.days as dayGroup}
+                        <div class="flex flex-col flex-grow">
+                          <div
+                            class="flex bg-purple-400 py-1 px-6 rounded-b-full"
+                          >
+                            <div
+                              class="text-sm sticky left-0 w-4 text-gray-800"
+                            >
+                              {dayGroup.day + 1}
                             </div>
                           </div>
-                        {/each}
-                      </div>
-                      </div>
-                    {/each}
-                  </div>
-                {/if}
-              </div>
-            {/each}
-          </div>
-        {/if}
-      </div>
-    {/each}
+                          <div class="flex flex-col space-x-2 pb-2 flex-grow">
+                            {#each dayGroup.notes as note}
+                              <div class="bg-white p-2 rounded-md w-80">
+                                <h3>{note.Dokumentnamn}</h3>
+                                <div class="text-xs text-gray-500">
+                                  {note.Dokument_skapad_av_yrkestitel_Namn}
+                                </div>
+                              </div>
+                            {/each}
+                          </div>
+                        </div>
+                      {/each}
+                    </div>
+                  {/if}
+                </div>
+              {/each}
+            </div>
+          {/if}
+        </div>
+      {/each}
+    </div>
   </div>
 </div>
