5e5bb571f035c5119f540f96b83eb0682ba6082d
GranneJanne <Wilja141@student.liu.se>
2025-04-17 11:51:03 +0200

    Working with filtering

---
+++ src/lib/components/NotePreview.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+  import { filter } from "$lib/stores";
+  import type { Note } from "$lib/models";
+  export let note;
+
+  const shapeMap: Record<
+    "Vårdenhet" | "Journalmall" | "Yrkesroll",
+    "Circle" | "Triangle" | "Square"
+  > = {
+    Vårdenhet: "Circle",
+    Journalmall: "Triangle",
+    Yrkesroll: "Square",
+  };
+
+  const colorMap: Record<
+    "Vårdenhet" | "Journalmall" | "Yrkesroll",
+    Record<string, string>
+  > = {
+    Vårdenhet: {
+      "Karolinska ÖV": "bg-orange-500",
+      "Privat ÖV": "bg-lime-500",
+      "Karolinska SV": "bg-emerald-500",
+      "SLSO ÖV": "bg-sky-500",
+      "Visby ÖV": "bg-fuchsia-500",
+      "Öppenvårdsmott. Urologi": "bg-cyan-500",
+    },
+    Journalmall: {
+      "Levnadsvanor": "text-rose-500",
+      "Nybesök Vårdcentral": "text-blue-500",
+      "Utskrivning-omvårdnad": "text-green-500",
+      "Inskrivningsanteckning SSK": "text-yellow-500",
+      "Intagningsanteckning": "text-purple-500",
+      "Mottagningsanteckning diabetes barn": "text-pink-500",
+      "Nybesöksanteckning": "text-orange-500",
+      "Remissbedömning": "text-indigo-500",
+    },
+    Yrkesroll: {
+      "Läkare": "bg-purple-500",
+      "Sjuksköterska": "bg-pink-500",
+    },
+  };
+
+  function getNoteProperty(note: Note, key: string) {
+    switch (key) {
+      case "Vårdenhet":
+        return note.Vårdenhet_Namn;
+      case "Journalmall":
+        return note.Dokumentnamn;
+      case "Yrkesroll":
+        return note.Dokument_skapad_av_yrkestitel_Namn;
+      default:
+        return null;
+    }
+  }
+
+  let matchingIndicators: Array<{
+    key: string;
+    shape: "Circle" | "Triangle" | "Square";
+    color: string;
+  }> = [];
+  $: matchingIndicators = [];
+
+  $: if ($filter) {
+    matchingIndicators = [];
+
+    for (const [key, activeValues] of $filter.entries()) {
+      if (activeValues.size === 0) continue;
+
+      const noteValue = getNoteProperty(note, key);
+      if (noteValue !== null && activeValues.has(noteValue)) {
+        matchingIndicators.push({
+          key,
+          shape: shapeMap[key as "Vårdenhet" | "Journalmall" | "Yrkesroll"],
+          color:
+            colorMap[key as "Vårdenhet" | "Journalmall" | "Yrkesroll"]?.[
+              noteValue
+            ] || "bg-gray-400",
+        });
+      }
+    }
+  }
+</script>
+
+
+<div class="flex items-center space-x-1 ml-2">
+{#each matchingIndicators as indicator}
+    {#if indicator.shape === "Circle"}
+    <div class={`w-2 h-2 rounded-full ${indicator.color}`}></div>
+    {:else if indicator.shape === "Triangle"}
+    <div
+        class={`w-0 h-0 border-l-4 border-r-4 border-b-8 border-transparent border-b-current ${indicator.color}`}
+    ></div>
+    {:else if indicator.shape === "Square"}
+    <div class={`w-2 h-2 ${indicator.color}`}></div>
+    {/if}
+{/each}
+</div>
+
--- src/lib/components/Timeline.svelte
+++ src/lib/components/Timeline.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
-  import { flip } from "svelte/animate";
   import { writable } from "svelte/store";
+  import NotePreview from "./NotePreview.svelte";
 
   import type { Note, Year, Month } from "$lib/models";
   import { buildDateHierarchy } from "$lib/utils";
-  import { allNotes, selectedNotes } from "$lib/stores";
+  import { filteredNotes, selectedNotes } from "$lib/stores";
 
-  const noteHierarchy = writable<Year[]>(buildDateHierarchy($allNotes));
+  const noteHierarchy = writable<Year[]>([]);
+
+  $: {
+    noteHierarchy.set(buildDateHierarchy($filteredNotes));
+  }
 
   function toggleGroup(group: Year | Month) {
     group.isCollapsed = !group.isCollapsed;
@@ -45,40 +49,55 @@
             {yearGroup.year}
           </div>
         </button>
-        <div class="flex flex-grow {yearGroup.isCollapsed ? 'flex-col' : 'flex-row'} overflow-y-hidden space-x-[2px]">
-        {#each yearGroup.months as monthGroup}
-          <div class="flex flex-col min-w-80">
-            <button
-              class="{yearGroup.isCollapsed ? 'hidden' : 'flex'} bg-purple-300 py-1 px-1 justify-between w-full shadow-xs"
-              on:click={() => toggleGroup(monthGroup)}
-              aria-label="Toggle month {monthGroup.month}"
-            >
-              <div class="text-sm sticky left-1 px-1 w-20 font-semibold text-gray-900 text-left">
-                {new Date(0, monthGroup.month).toLocaleString("default", {
-                  month: "long",
-                })}
-              </div>
-            </button>
-              <div class="relative flex {monthGroup.isCollapsed ? 'flex-col' : 'flex-row'} overflow-y-hidden p-[2px] gap-[4px]">
+        <div
+          class="flex flex-grow {yearGroup.isCollapsed
+            ? 'flex-col'
+            : 'flex-row'} overflow-y-hidden space-x-[2px]"
+        >
+          {#each yearGroup.months as monthGroup}
+            <div class="flex flex-col min-w-80">
+              <button
+                class="{yearGroup.isCollapsed
+                  ? 'hidden'
+                  : 'flex'} bg-purple-300 py-1 px-1 justify-between w-full shadow-xs"
+                on:click={() => toggleGroup(monthGroup)}
+                aria-label="Toggle month {monthGroup.month}"
+              >
+                <div
+                  class="text-sm sticky left-1 px-1 w-20 font-semibold text-gray-900 text-left"
+                >
+                    {new Date(0, monthGroup.month).toLocaleString("sv-SE", {
+                    month: "long",
+                  })}
+                </div>
+              </button>
+              <div
+                class="relative flex {monthGroup.isCollapsed
+                  ? 'flex-col'
+                  : 'flex-row'} overflow-y-hidden p-[2px] gap-[4px]"
+              >
                 {#each monthGroup.notes as note}
-                {#if yearGroup.isCollapsed}
-                  <button
-                    class="flex bg-white py-1 px-2 justify-between w-full rounded-md shadow-xs border-1 border-gray-200"
-                    on:click={() => handleNoteClick(note)}
-                    aria-label="Select note {note.Dokument_ID}"
-                  >
-                  <div class="text-gray-900 font-semibold text-left text-xs">
-                    {note.Dokumentnamn}
-                  </div>
-                  </button>
-                {:else if monthGroup.isCollapsed}
-                  <button
+                  {#if yearGroup.isCollapsed}
+                    <button
+                      class="flex bg-white py-1 px-2 justify-between w-full rounded-md shadow-xs border border-gray-200"
+                      on:click={() => handleNoteClick(note)}
+                      aria-label="Select note {note.Dokument_ID}"
+                    >
+                      <div
+                        class="text-gray-900 font-semibold text-left text-xs"
+                      >
+                        {note.Dokumentnamn}
+                      </div>
+                      <NotePreview {note} />
+                    </button>
+                  {:else if monthGroup.isCollapsed}
+                    <button
                       class="flex bg-white py-1 px-2 outline-1 outline-gray-100 flex-col w-full text-sm text-left border-1 border-gray-200"
                       on:click={() => handleNoteClick(note)}
                       aria-label="Select note {note.Dokument_ID}"
                     >
-                      <div class="text-gray-900 font-semibold">
-                        {note.Dokumentnamn}
+                      <div class="text-gray-900 font-semibold flex justify-between">
+                        {note.Dokumentnamn} <NotePreview {note} />
                       </div>
                       <div class="text-gray-800 text-xs">
                         {new Date(note.DateTime).toLocaleDateString("sv-SE")}
@@ -86,23 +105,25 @@
                       <div class="text-gray-800 text-xs">
                         {note.Dokument_skapad_av_yrkestitel_Namn}
                       </div>
-                  </button>
-                {:else}
-                  <button
-                    class="flex bg-white p-2 outline-1 outline-gray-100 justify-between w-full h-full overflow-y-auto border-1 border-gray-200"
-                    on:click={() => handleNoteClick(note)}
-                    aria-label="Select note {note.Dokument_ID}"
-                  >
-                    <div class="text-gray-900 min-w-80 text-left text-xs">
-                      {@html note.CaseData}
-                    </div>
-                  </button>
-                {/if}
+                    </button>
+                  {:else}
+                    <button
+                      class="flex bg-white p-2 outline-1 outline-gray-100 justify-between w-full h-full overflow-y-auto border-1 border-gray-200"
+                      on:click={() => handleNoteClick(note)}
+                      aria-label="Select note {note.Dokument_ID}"
+                    >
+                      <div
+                        class="text-gray-900 min-w-80 max-w-120 text-left text-xs"
+                      >
+                        {@html note.CaseData}
+                      </div>
+                    </button>
+                  {/if}
                 {/each}
               </div>
-          </div>
-        {/each}
-      </div>
+            </div>
+          {/each}
+        </div>
       </div>
     {/each}
   </div>
